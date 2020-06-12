1b7e47b3ddee8282020a8d4063276fe38a1873db
Diansulistyarini <55771328+Diansulistyarini@users.noreply.github.com>
2020-06-13 05:06:39 +0700

    Pembuatan Bot Telegram dengan Python

---
+++ Mybot.py
@@ -0,0 +1,52 @@
+import telebot
+import mysql.connector
+
+import mytoken
+
+
+from datetime import datetime
+TOKEN=mytoken.TOKEN
+myBot = telebot.TeleBot(TOKEN)
+myDb=mysql.connector.connect(host='localhost',user='root',database='db_belajarbot')
+sql=myDb.cursor()
+from telebot import apihelper
+waktusekarang=datetime.now()
+
+class Mybot:
+    def __init__(self):
+        self.message
+
+    @myBot.message_handler(commands=['start', 'help'])
+    def start(message):
+        # photo = open('img/rpl.png', 'rb')
+        # myBot.send_photo(message.from_user.id, photo)
+        teks = mytoken.SAPA + "\n-- admin & developer @diansulistyarini - SMK Taruna Bhakti -- "+"\n" \
+                        "hari ini tanggal "+str(waktusekarang)
+        myBot.reply_to(message, teks)
+
+    @myBot.message_handler(commands=['datasiswa'])
+    def menu_data_siswa(message):
+        query="select nipd,nama,kelas from tabel_siswa "
+        sql.execute(query)
+        data=sql.fetchall()
+        jmldata = sql.rowcount
+        kumpuldata=''
+        if(jmldata>0):
+            #print(data)
+            no=0
+            for x in data:
+                no += 1
+                kumpuldata =kumpuldata+ str(x)
+                print(kumpuldata)
+                kumpuldata = kumpuldata.replace('(', '')
+                kumpuldata = kumpuldata.replace(')', '')
+                kumpuldata = kumpuldata.replace("'", '')
+                kumpuldata = kumpuldata.replace(",", '')
+        else:
+            print('data kosong')
+
+        myBot.reply_to(message,str(kumpuldata))
+
+print(myDb)
+print("-- Bot sedang berjalan --")
+myBot.polling(none_stop=True)
---
+++ mytoken.py
@@ -0,0 +1,2 @@
+TOKEN='1264689495:AAET8JBgCt8fw4K5-JHeQfZUC0IbMSvoB4k'
+SAPA='Hallo Saya Bot Starbhak Assistant'
